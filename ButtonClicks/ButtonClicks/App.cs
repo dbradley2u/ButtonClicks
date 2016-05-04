38de9e30ccddbafada4aaf81cc48cc8f62fd06eb
Denise <denise.bradley@baker.edu>
2016-05-03 23:52:03 -0400

    Chapter 6 through page 120 initial commit.

--- ButtonClicks/ButtonClicks/App.cs
+++ ButtonClicks/ButtonClicks/App.cs
@@ -11,20 +11,7 @@ namespace ButtonClicks
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new ButtonLambdasPage();
         }
 
         protected override void OnStart()
